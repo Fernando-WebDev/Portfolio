c62d2c7f354ff09da92d6c9b299380828465f797
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-07-20 15:15:07 -0300

    mudei o design

--- projeto/script.js
+++ projeto/script.js
@@ -2,7 +2,7 @@
         // Smooth scrolling
         document.querySelectorAll('a[href^="#]').forEach(anchor => {
             anchor.addEventListener('click', funtion (e) {
-                e.preventDefault();
+                e,preventDefault();
                 document.querySelector(this.getAttribute('href')).scrollIntoView({
                     behavior: 'smooth'
                 });
